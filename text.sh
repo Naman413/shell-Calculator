e944d10b3d365c365045d13f84467b042ec79709
Naman Goel <NamanGoel@123gmail.com>
2026-05-10 08:24:08 +0000

    Addition fun were added

--- text.sh
+++ text.sh
@@ -8,13 +8,17 @@
 
 
 read  -p "Enter the number1:  " num1
-read -p  "Enter the mumber2: " num2
+read -p  "Enter the number2: " num2
 
+echo "For Add press (1) || for Sub press (2) || for multiple press (3) || for division (4)"
+read -p " enter what u want to do " num3
 function calci(){
-	num3=$((num1 + num2))
-	echo "sum = $num3"
+	if [[ $num3 == 1 ]]
+	then
+		echo "sum is  $((num1 + num2))"
+	fi
+
 
 }
-
 calci
 
